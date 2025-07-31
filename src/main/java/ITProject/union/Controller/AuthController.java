c42a0a904e21497e5144c9a1bcc061c23fd5cfcc
chaBee <jhcha990917@gmail.com>
2025-07-31 18:33:50 +0900

    jh 1 commit

--- src/main/java/ITProject/union/Controller/AuthController.java
+++ src/main/java/ITProject/union/Controller/AuthController.java
@@ -7,6 +7,8 @@ import ITProject.union.Dto.TokenResponse;
 import ITProject.union.Entity.User;
 import ITProject.union.Security.CustomUserDetails;
 import ITProject.union.Service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "회원인증 API")
 public class AuthController {
 
     private final AuthService authService;
@@ -26,6 +29,7 @@ public class AuthController {
      * 📝 회원가입
      */
     @PostMapping("/signup")
+    @Operation(summary="회원가입")
     public ResponseEntity<String> signup(@RequestBody SignupRequest request) {
         authService.signup(request);
         return ResponseEntity.ok("회원가입 완료");
@@ -35,6 +39,7 @@ public class AuthController {
      * 🔑 로그인: AccessToken + RefreshToken 발급
      */
     @PostMapping("/signin")
+    @Operation(summary="로그인")
     public ResponseEntity<TokenResponse> signin(@RequestBody SigninRequest request) {
         TokenResponse response = authService.signin(request);
         return ResponseEntity.ok(response);
@@ -44,6 +49,7 @@ public class AuthController {
      * 🔄 토큰 재발급
      */
     @PostMapping("/reissue")
+    @Operation(summary="토큰재발급")
     public ResponseEntity<TokenResponse> reissue(@RequestBody TokenReissueRequest request) {
         TokenResponse response = authService.reissueToken(request);
         return ResponseEntity.ok(response);
@@ -53,6 +59,7 @@ public class AuthController {
      * 🚪 로그아웃: Refresh Token 삭제
      */
     @PostMapping("/logout")
+    @Operation(summary="로그아웃")
     public ResponseEntity<String> logout(@AuthenticationPrincipal CustomUserDetails userDetails) {
         authService.logout(userDetails.getUser());
         return ResponseEntity.ok("로그아웃 완료");
--- src/main/java/ITProject/union/Controller/SubjectController.java
+++ src/main/java/ITProject/union/Controller/SubjectController.java
@@ -4,6 +4,8 @@ import ITProject.union.Dto.SubjectRequestDto;
 import ITProject.union.Dto.SubjectResponseDto;
 import ITProject.union.Security.CustomUserDetails;
 import ITProject.union.Service.SubjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,12 +16,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/subjects")
 @RequiredArgsConstructor
+@Tag(name = "과목 API")
 public class SubjectController {
 
     private final SubjectService subjectService;
 
     // 과목 등록
     @PostMapping
+    @Operation(summary="과목 추가")
     public ResponseEntity<Long> createSubject(@RequestBody SubjectRequestDto request,
                                               @AuthenticationPrincipal CustomUserDetails user) {
         Long id = subjectService.createSubject(user.getId(), request);
@@ -28,6 +32,7 @@ public class SubjectController {
 
     // 전체 과목 조회
     @GetMapping
+    @Operation(summary="전체 과목 조회")
     public ResponseEntity<List<SubjectResponseDto>> getSubjects(@AuthenticationPrincipal CustomUserDetails user) {
         System.out.println("👉 로그인한 사용자 ID: " + user.getId());
         return ResponseEntity.ok(subjectService.getSubjects(user.getId()));
@@ -35,6 +40,7 @@ public class SubjectController {
 
     // 과목 수정
     @PutMapping("/{id}")
+    @Operation(summary="과목 수정")
     public ResponseEntity<Void> updateSubject(@PathVariable Long id,
                                               @RequestBody SubjectRequestDto request,
                                               @AuthenticationPrincipal CustomUserDetails user) {
@@ -44,6 +50,7 @@ public class SubjectController {
 
     // 과목 삭제
     @DeleteMapping("/{id}")
+    @Operation(summary="과목 삭제")
     public ResponseEntity<Void> deleteSubject(@PathVariable Long id,
                                               @AuthenticationPrincipal CustomUserDetails user) {
         subjectService.deleteSubject(user.getId(), id);
