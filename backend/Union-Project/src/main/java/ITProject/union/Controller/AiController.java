34a14d5a5c3cc81118383f5a8db11ed1b2a34b6f
chabee <jhcha990917@gmail.com>
2025-08-09 00:39:49 +0900

    ãswagger 추가

--- backend/Union-Project/src/main/java/ITProject/union/Controller/AiController.java
+++ backend/Union-Project/src/main/java/ITProject/union/Controller/AiController.java
@@ -2,6 +2,8 @@ package ITProject.union.Controller;
 
 import ITProject.union.Dto.Ai.PdfSummaryResponse;
 import ITProject.union.Service.Ai.AiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +12,11 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/ai")
 @RequiredArgsConstructor
+@Tag(name = "Ai 기능 API")
 public class AiController {
 
     private final AiService aiService;
-
+    @Operation(summary = "pdf 요약")
     @PostMapping("/pdfs/summary")
     public ResponseEntity<PdfSummaryResponse> summarizePdf(@RequestParam("file") MultipartFile file) {
         System.out.println("🔥🔥🔥 PDF 요약 API 진입했음");
