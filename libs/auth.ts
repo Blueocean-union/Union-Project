bbbf4060f809e751b210a05eb29df0d244d04590
chabee <109027671+JunHyeok-Cha@users.noreply.github.com>
2025-09-02 11:36:27 +0900

    Revert "My changes"

--- libs/auth.ts
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-export const loginUser = async (email: string, password: string) => {
-  const response = await api.post('/auth/signin', {
-    email,
-    password,
-  });
-  return response.data; // accessToken, refreshToken 포함
-};
