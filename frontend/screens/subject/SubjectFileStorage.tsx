fbb4495f250acf2bed23fe567e3f8a5cdf3b2786
KYM <todbals1@naver.com>
2025-09-12 16:13:27 +0900

    legacy로 수정

--- frontend/screens/subject/SubjectFileStorage.tsx
+++ frontend/screens/subject/SubjectFileStorage.tsx
@@ -14,7 +14,7 @@ import {
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as DocumentPicker from 'expo-document-picker';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import * as Sharing from 'expo-sharing';
 import { Audio } from 'expo-av';
 import AsyncStorage from '@react-native-async-storage/async-storage';
